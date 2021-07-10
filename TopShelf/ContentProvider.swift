495a9dbedb246496c0a0334f851b042030f800a9
Stephen Byatt <47413006+stevebyatt10@users.noreply.github.com>
2021-07-10 14:19:29 +1000

    target fixes

--- TopShelf/ContentProvider.swift
+++ TopShelf/ContentProvider.swift
@@ -1,26 +1,18 @@
-import Combine
+//
+//  ContentProvider.swift
+//  TopShelf
+//
+//  Created by Stephen Byatt on 10/7/21.
+//
+
 import TVServices
-import JellyfinAPI
 
 class ContentProvider: TVTopShelfContentProvider {
 
-    func getContinueWatching() {
-        let server = ServerEnvironment.current.server
-        let savedUser = SessionManager.current.user
-        var tempCancellables = Set<AnyCancellable>()
-    }
-
     override func loadTopShelfContent(completionHandler: @escaping (TVTopShelfContent?) -> Void) {
         // Fetch content and call completionHandler
-        let continueWatching = TVTopShelfSectionedItem(identifier: "ContinueWatchibng")
-        continueWatching.imageShape = .poster
-        continueWatching.title = "Test"
-
-        let collection = TVTopShelfItemCollection(items: [continueWatching])
-        let content = TVTopShelfSectionedContent(sections: [collection])
-
-        completionHandler(content)
-
+        completionHandler(nil);
     }
 
 }
+
